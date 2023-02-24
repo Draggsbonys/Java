ddccc6ff66c974b58e94a6365172db80ea56ae48
sergeyshmidt <draggsbony@gmail.com>
2023-02-24 18:17:54 +0300

    123

---
+++ src/Equals/Car.java
@@ -0,0 +1,37 @@
+package Equals;
+/*
+ * Метод equals() в Java используется для сравнения объектов на равенство.
+ * Он определен в классе Object, который является базовым классом для всех классов в Java.
+ * По умолчанию метод equals() в классе object сравнивает ссылки на объекты
+ * И для корректного сравнения объектов между собой, он должен быть перезаписан.
+ *
+ * Если вы перезаписываете метод equals(), всегда используется в его параметре data type Object
+ *
+ * Правильно и логично перезаписанный метод equals() должен обладать следующими свойствами:
+ * Симметричность - для non-null ссылочных переменных a и b, a.equals(b) возвращает true если b.equals(a) возвращает true
+ * Рефлексивность - для non-null ссылочной переменной а, а.equals(a) всегда должно возвращать true
+ * Транзитивность - для non-null ссылочных переменных a, b и с, если a.equals(b) и b.equals(c) возвращает true, то a.equals(c) тоже должно возвращать true
+ * Постоянство - для non-null ссылочных переменных a и b, неоднократный вызов a.equals(b) должен возвращать или только true, или только false
+ * Для non-null ссылочной переменной а. a.equals(null) всегда должно возвращать false
+ *
+*/
+public class Car {
+    String color;
+    String engine;
+
+    Car(String color, String engine) {
+        this.color = color;
+        this.engine = engine;
+    }
+
+    // Пример перезаписи метода equals
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Car) {
+            Car car = (Car) obj;
+            return (color.equals(car.color) && engine.equals(car.engine));
+        } else {
+            return false;
+        }
+    }
+}
---
+++ src/toString/Main.java
@@ -0,0 +1,8 @@
+package toString;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal("Rex", 38);
+        System.out.println(animal);
+    }
+}
