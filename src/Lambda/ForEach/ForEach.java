20d5041df7ce37ef150086b741a98e62026788ac
sergeyShmidt <draggsbony@gmail.com>
2023-04-05 17:01:26 +0300

    05.04.2023

--- src/Lambda/ForEach/ForEach.java
+++ src/Lambda/ForEach/ForEach.java
@@ -4,10 +4,21 @@ import java.util.List;
     Интерфейс forEach() это метод, который объявлен в интерфейсе Iterable<T> и реализуется во многих классах коллекций
     (например, List, Set, Map и других). Он позволяет применять заданную операцию к каждому элементу коллекции.
 */
-public class Main {
+public class ForEach {
     public static void main(String[] args) {
         List<String> list = List.of("Hello", "Red", "One");
 
-        list.forEach(str -> System.out.println(str));
+        // Краткий вариант записи вывода в консоль
+        list.forEach(System.out::println);
+
+        // В параметр forEach можно передавать методы
+        list.forEach(Utils::myMethod);
+    }
+}
+
+class Utils {
+    public static void myMethod(String str) {
+        str = str + "1";
+        System.out.println(str);
     }
 }
